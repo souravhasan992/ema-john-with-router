32941f7db1900ad7d611d040706d233a1457b422
souravhasan992 <souravhasan992@gmail.com>
2021-10-18 12:42:47 +0600

    cart and order review added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <h3>The page you are looking for is not availabe</h3>
+        </div>
+    );
+};
+
+export default NotFound;
